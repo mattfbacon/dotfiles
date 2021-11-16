f4bb17cb7153acd6128d7e09c7c46b00df98d9cc
Matt Fellenz <mattf53190@gmail.com>
2021-11-16 08:19:10 -0800

    Scripts: use tmux inside mosh

--- fish/.config/fish/functions/srv.fish
+++ fish/.config/fish/functions/srv.fish
@@ -1,3 +1,3 @@
 function srv --wraps='ssh support@mynfs -p 69'
-  mosh --ssh="ssh -p 69" support@mynfs
+  mosh --ssh="ssh -p 69" support@mynfs -- sh -c 'tmux has-session -t 0 && tmux attach -t 0 -d || tmux'
 end
--- fish/.config/fish/functions/vps.fish
+++ fish/.config/fish/functions/vps.fish
@@ -1,3 +1,3 @@
 function vps --wraps='ssh matt@myvps'
-  mosh matt@mattfellenz.be
+  mosh matt@mattfellenz.be -- sh -c 'tmux has-session -t 0 && tmux attach -t 0 -d || tmux'
 end
