c4c3658a81e782f8829391898a7ac3305c7db977
Matt Fellenz <mattf53190@gmail.com>
2021-11-19 12:29:49 -0800

    Scripts: backup.sh: fix typo

--- scripts/.local/bin/backup.sh
+++ scripts/.local/bin/backup.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
 export BORG_REPO="ssh://support@mynfs:69//srv/backup"
+STTY_ORIG=`stty -g`
+stty -echo
 echo -n 'passphrase: '
 read BORG_PASSPHRASE
+echo
+stty $STTY_ORIG
 export BORG_PASSPHRASE
 
 echo 'Starting backup'
 
-borg create --verbose --filter AME --list --stats --show-rc --compression lzma --exclude-caches --exclude ~/.cache --exclude ~/dotfiles --exclude ~/.deja-dup --exclude ~/Documents/ISOs --exclude ~/.cargo --exclude ~/go --exclude ~/.npm --exclude ~/.rustup --exclude ~/.config/BraveSoftware/Brave-Browser --exclude ~/.local/lib --exclude ~/.local/share --exclude ~/.opam --exclude ~/.steam --exclude ~/.var --exclude ~/.vmodules ---exclude ~/.vpython-root --exclude ~/.themes --exclude ~/.cabal --exclude ~/.ghcup --exclude ~/.tack --exclude ~/.cpanm --exclude ~/.emscripten_cache --exclude ~/.surf ::'{hostname}-{now}' ~ || exit 1
+borg create --verbose --filter AME --list --stats --show-rc --compression lzma --exclude-caches --exclude ~/.cache --exclude ~/dotfiles --exclude ~/.deja-dup --exclude ~/Documents/ISOs --exclude ~/.cargo --exclude ~/go --exclude ~/.npm --exclude ~/.rustup --exclude ~/.config/BraveSoftware/Brave-Browser --exclude ~/.local/lib --exclude ~/.local/share --exclude ~/.opam --exclude ~/.steam --exclude ~/.var --exclude ~/.vmodules --exclude ~/.vpython-root --exclude ~/.themes --exclude ~/.cabal --exclude ~/.ghcup --exclude ~/.tack --exclude ~/.cpanm --exclude ~/.emscripten_cache --exclude ~/.surf ::'{hostname}-{now}' ~ || exit 1
 
 echo 'Backup complete, starting prune'
 
