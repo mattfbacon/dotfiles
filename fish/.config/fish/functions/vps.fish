caf6329e7aa957c570f968273359aab023204dac
Matt Fellenz <mattf53190@gmail.com>
2021-11-27 15:46:34 -0800

    Scripts: update hostname for vps

--- fish/.config/fish/functions/vps.fish
+++ fish/.config/fish/functions/vps.fish
@@ -1,3 +1,3 @@
 function vps --wraps='ssh matt@myvps'
-  mosh matt@mattfellenz.be -- sh -c 'SESS=$(tmux list-sessions | grep -v attached | head -1 | cut -d':' -f1); test -n "$SESS" && tmux attach -t "$SESS" -d || tmux'
+  mosh matt@felle.nz -- sh -c 'SESS=$(tmux list-sessions | grep -v attached | head -1 | cut -d':' -f1); test -n "$SESS" && tmux attach -t "$SESS" -d || tmux'
 end
