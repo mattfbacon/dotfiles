88e353a00e182966862f2a3fdca8b4388ba32a90
Matt Fellenz <mattf53190@gmail.com>
2021-11-27 15:45:03 -0800

    Scripts: make tmux attachment smarter

--- fish/.config/fish/functions/srv.fish
+++ fish/.config/fish/functions/srv.fish
@@ -1,3 +1,3 @@
 function srv --wraps='ssh support@mynfs -p 69'
-  mosh --ssh="ssh -p 69" support@mynfs -- sh -c 'tmux has-session -t 0 && tmux attach -t 0 -d || tmux'
+  mosh --ssh="ssh -p 69" support@mynfs -- sh -c 'SESS=$(tmux list-sessions | grep -v attached | head -1 | cut -d':' -f1); test -n "$SESS" && tmux attach -t "$SESS" -d || tmux'
 end
--- fish/.config/fish/functions/vps.fish
+++ fish/.config/fish/functions/vps.fish
@@ -1,3 +1,3 @@
 function vps --wraps='ssh matt@myvps'
-  mosh matt@mattfellenz.be -- sh -c 'tmux has-session -t 0 && tmux attach -t 0 -d || tmux'
+  mosh matt@mattfellenz.be -- sh -c 'SESS=$(tmux list-sessions | grep -v attached | head -1 | cut -d':' -f1); test -n "$SESS" && tmux attach -t "$SESS" -d || tmux'
 end
