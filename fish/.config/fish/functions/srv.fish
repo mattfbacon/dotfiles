ef86839faa4a6bc1a024e142f33e94d113798dd3
Matt Fellenz <mattf53190@gmail.com>
2021-11-29 15:16:34 -0800

    Scripts: update srv conn details

--- fish/.config/fish/functions/srv.fish
+++ fish/.config/fish/functions/srv.fish
@@ -1,3 +1,3 @@
-function srv --wraps='ssh support@mynfs -p 69'
-  mosh --ssh="ssh -p 69" support@mynfs -- sh -c 'SESS=$(tmux list-sessions | grep -v attached | head -1 | cut -d':' -f1); test -n "$SESS" && tmux attach -t "$SESS" -d || tmux'
+function srv --wraps='ssh support@ark.matt.felle.nz'
+  mosh support@ark.matt.felle.nz -- sh -c 'SESS=$(tmux list-sessions | grep -v attached | head -1 | cut -d':' -f1); test -n "$SESS" && tmux attach -t "$SESS" -d || tmux'
 end
