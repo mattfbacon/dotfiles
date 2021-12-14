4a986ad92979cbdb368a28b98bb9ce66edafcd51
Matt Fellenz <mattf53190@gmail.com>
2021-12-14 08:12:38 -0800

    Scripts: convert cleancomputer.sh to Wayland

--- scripts/.local/bin/cleancomputer.sh
+++ scripts/.local/bin/cleancomputer.sh
@@ -3,15 +3,16 @@ if test -z $1; then
   echo 'Usage: cleancomputer.sh <time to disable>'
   exit 1
 fi
-INPUTS=$(xinput --list | awk -F'\t' 'BEGIN{IGNORECASE=1};/(AT Translated Set 2 keyboard|touchpad|finger).+slave/ { print $2 }' | cut -d'=' -f2)
-for dv in $INPUTS; do
-  xinput disable $dv
-done
+swaymsg input '*' events disabled
+swaymsg input '*' events disabled
+swaymsg input '*' events disabled
+swaymsg input '*' events disabled
 sleep $(($1 - 3))
 for i in $(seq 3 -1 1); do
   echo 'reenabling in '$i
   sleep 1
 done
-for dv in $INPUTS; do
-  xinput enable $dv
-done
+swaymsg input '*' events enabled
+swaymsg input '*' events enabled
+swaymsg input '*' events enabled
+swaymsg input '*' events enabled
