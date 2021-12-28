f325dc9a0032cc24c3317561cd90e53c57043085
Matt Fellenz <mattf53190@gmail.com>
2021-12-28 09:23:16 -0500

    Scripts: enhance scrot.sh

--- scripts/.local/bin/scrot.sh
+++ scripts/.local/bin/scrot.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
-TYPE="$(echo -e 'screen\narea' | bemenu)"
 
-case "$TYPE" in
-	screen)
-		grim
+default_location() {
+	echo ~/Pictures/grim_"$(date +%F_%H-%M-%S)".png
+}
+
+TYPE="$(echo -e 'screen\narea' | bemenu -p Type)"
+OUTPUT="$(echo -e 'file\nclipboard' | bemenu -p Output)"
+
+REGION=""
+test "$TYPE" = area && REGION="$(slurp)"
+
+case "$OUTPUT" in
+	file)
+		grim ${REGION:+-g "$REGION"} "$(default_location | bemenu -p Location)"
 		;;
-	area)
-		grim -g "$(slurp)"
+	clipboard)
+		grim ${REGION:+-g "$REGION"} - | wl-copy -t image/png
 		;;
 	*)
 		exit 1
