176a0dedeae70e194d8b25bbd6e4a7c0391e97b7
Matt Fellenz <mattf53190@gmail.com>
2022-01-08 08:59:29 -0800

    Srv: change URL

--- fish/.config/fish/functions/srv.fish
+++ fish/.config/fish/functions/srv.fish
@@ -1,3 +1,3 @@
 function srv --wraps='ssh support@ark.matt.felle.nz'
-  mosh support@ark.matt.felle.nz -- sh -c 'SESS=$(tmux list-sessions | grep -v attached | head -1 | cut -d':' -f1); test -n "$SESS" && tmux attach -t "$SESS" -d || tmux'
+  mosh support@mc.matt.felle.nz -- sh -c 'SESS=$(tmux list-sessions | grep -v attached | head -1 | cut -d':' -f1); test -n "$SESS" && tmux attach -t "$SESS" -d || tmux'
 end
