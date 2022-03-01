0c68abda7bd9f65f6f77f34390d243e0ce322ecc
Matt Fellenz <matt@felle.nz>
2022-03-01 15:13:15 -0800

    Scripts: switch to Firefox

--- scripts/.local/bin/browse.sh
+++ scripts/.local/bin/browse.sh
@@ -4,13 +4,16 @@ PROFILE_DIR="$1"
 
 case "$PROFILE_DIR" in
 	school)
-		PROFILE_DIR="Profile 3"
+		# PROFILE_DIR="Profile 3"
+		PROFILE_DIR="0td0hysr.School"
 		;;
 	home)
-		PROFILE_DIR="Profile 2"
+		# PROFILE_DIR="Profile 2"
+		PROFILE_DIR="5bmfmi8v.Home"
 		;;
 	college)
-		PROFILE_DIR="Profile 1"
+		# PROFILE_DIR="Profile 1"
+		PROFILE_DIR="mh4f9c7h.College"
 		;;
 	*)
 		echo "Invalid profile dir $PROFILE_DIR" >&2
@@ -18,6 +21,8 @@ case "$PROFILE_DIR" in
 		;;
 esac
 
+PROFILE_DIR=~/.mozilla/firefox/"$PROFILE_DIR"
 shift
 
-exec brave --enable-features=UseOzonePlatform,WebRTCPipeWireCapture --ozone-platform=wayland --profile-directory="$PROFILE_DIR" $@
+# exec brave --enable-features=UseOzonePlatform,WebRTCPipeWireCapture --ozone-platform=wayland --profile-directory="$PROFILE_DIR" $@
+exec firefox --profile "$PROFILE_DIR" $@
