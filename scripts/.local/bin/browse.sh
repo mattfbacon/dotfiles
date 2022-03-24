882293727ed81f1cc35e4ecb04294d62313ced31
Matt Fellenz <matt@felle.nz>
2022-03-24 09:34:21 -0700

    Add browse.sh and use it for `$BROWSER`
    First of all, create a `browse.sh` script to simplify launching the browser with various profiles and in various modes.
    
    Second, `xdg-open` contains logic that prevents what they call "argument injection" by removing any arguments from `$BROWSER`, so create a proxy script called `browse-home.sh` that passes some arguments to `browse.sh` before the arguments it was given. Now `xdg-open` works properly.

---
+++ scripts/.local/bin/browse.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+PROFILE=""
+case "$1" in
+	home)
+		PROFILE="st2fjblt.Home"
+	;;
+	school)
+		PROFILE="35gt8ti4.School"
+	;;
+	college)
+		PROFILE="l4xgjvs4.College"
+	;;
+	*)
+		echo 'Unknown profile' >&2
+		exit 1
+	;;
+esac
+shift
+
+ARGS=""
+case "$1" in
+	normal)
+		ARGS=""
+	;;
+	tab)
+		ARGS="--new-tab"
+	;;
+	window)
+		ARGS="--new-window"
+	;;
+	browse)
+		ARGS="--browser"
+	;;
+	private)
+		ARGS="--private-window"
+	;;
+	*)
+		echo 'Unknown browser type' >&2
+		exit 1
+	;;
+esac
+shift
+
+exec firefox --profile "$HOME/.mozilla/firefox/$PROFILE" $ARGS "$@"
