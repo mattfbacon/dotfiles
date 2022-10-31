7af6ba65986456e0967950b3a721a810394147d3
Matt Fellenz <matt@felle.nz>
2022-10-31 10:59:20 -0700

    Extract and update disk UUIDs

--- scripts/.local/bin/privpriv.sh
+++ scripts/.local/bin/privpriv.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-udisksctl unlock -b /dev/disk/by-uuid/08fcb094-3917-427b-af9c-05415e8d060c || exit $?
-trap 'udisksctl lock -b /dev/disk/by-uuid/08fcb094-3917-427b-af9c-05415e8d060c || exit $?' EXIT
-udisksctl mount -b /dev/disk/by-uuid/2a8b0782-8a64-4957-b033-81b5e23fc321 || exit $?
-trap 'udisksctl unmount -b /dev/disk/by-uuid/2a8b0782-8a64-4957-b033-81b5e23fc321 || exit $?; udisksctl lock -b /dev/disk/by-uuid/08fcb094-3917-427b-af9c-05415e8d060c || exit $?' EXIT
-fish --private -C 'cd /run/media/matt/predatni' || exit $?
+DISK=/dev/disk/by-uuid/f0b460c2-bfce-47c2-818e-8aacf1c9e31d
+INNERDISK=/dev/disk/by-uuid/eb458541-29f6-4af1-9260-027f4c684f03
+
+udisksctl unlock -b $DISK || exit $?
+trap 'udisksctl lock -b $DISK || exit $?' EXIT
+udisksctl mount -b $INNERDISK || exit $?
+trap 'udisksctl unmount -b $INNERDISK || exit $?; udisksctl lock -b $DISK || exit $?' EXIT
+fish --private -C 'cd /run/media/matt/sivydatni' || exit $?
 
