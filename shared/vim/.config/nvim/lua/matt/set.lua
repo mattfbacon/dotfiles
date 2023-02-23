e3a8c81c0e8c9837a818f6831f1002c0282a914b
Matt Fellenz <matt@felle.nz>
2023-02-23 12:55:40 -0800

    Vim: disable LSP logging

--- shared/vim/.config/nvim/lua/matt/set.lua
+++ shared/vim/.config/nvim/lua/matt/set.lua
@@ -79,3 +79,5 @@ sign define DiagnosticSignWarn text= texthl=DiagnosticSignWarn linehl= numhl=
 sign define DiagnosticSignInfo text= texthl=DiagnosticSignInfo linehl= numhl=
 sign define DiagnosticSignHint text= texthl=DiagnosticSignHint linehl= numhl=
 ]]
+
+vim.lsp.set_log_level 'off'
