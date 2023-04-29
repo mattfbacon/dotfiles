cf8be881f34cbf6c6757c170770e497c92548608
Matt Fellenz <matt@felle.nz>
2023-04-28 22:17:47 -0700

    Un-reunify browse.sh

---
+++ framework/scripts/.local/bin/browse.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+PROFILE=""
+case "$1" in
+	home)
+		PROFILE="3k9z7b16.default"
+	;;
+	school)
+		PROFILE="m8tvww85.School"
+	;;
+	*)
+		echo 'Unknown profile' >&2
+		exit 1
+	;;
+esac
+shift
+
+ARGS=""
+case "$1" in
+	normal)
+		ARGS=""
+	;;
+	tab)
+		ARGS="--new-tab"
+	;;
+	window)
+		ARGS="--new-window"
+	;;
+	browse)
+		ARGS="--browser"
+	;;
+	private)
+		ARGS="--private-window"
+	;;
+	*)
+		echo 'Unknown browser type' >&2
+		exit 1
+	;;
+esac
+shift
+
+exec firefox --profile "$HOME/.mozilla/firefox/$PROFILE" $ARGS "$@"
